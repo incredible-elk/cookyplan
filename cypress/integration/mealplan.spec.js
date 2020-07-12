8cf25cdf96b6fa6fccb2b9d04f4feaefa45407b9
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-07-12 22:17:37 +0200

    Apply pr feedback regarding cypress test

--- cypress/integration/mealplan.spec.js
+++ cypress/integration/mealplan.spec.js
@@ -1,49 +1,67 @@
-it('Creating a Mealplan List', () => {
-  cy.visit('/');
-  cy.get('[name="listtitle"]').type('Tuesday');
-  cy.get('[name="listtitle"]').should('have.value', 'Tuesday');
+describe('MealplanList', () => {
+  it('allows changing the title', () => {
+    cy.visit('/');
+    cy.get('[name="listtitle"]').type('Tuesday');
+    cy.get('[name="listtitle"]').should('have.value', 'Tuesday');
 
-  cy.get('#meals li').eq(0).type('Burritos');
-  cy.get('#meals li:eq(0) input[type=text]').should('have.value', 'Burritos');
+    cy.get('[name="listtitle"]').clear().type('Wednesday');
+    cy.get('[name="listtitle"]').should('have.value', 'Wednesday');
+  });
 
-  cy.get('[name="addbutton"]').click();
+  it('allows to add meals', () => {
+    cy.visit('/');
+    cy.get('#meals li').eq(0).type('Burritos');
+    cy.get('#meals li:eq(0) input[type=text]').should('have.value', 'Burritos');
 
-  cy.get('#meals li').eq(1).type('Falafel Wrap');
-  cy.get('#meals li:eq(1) input[type=text]').should(
-    'have.value',
-    'Falafel Wrap'
-  );
+    cy.get('[name="addbutton"]').click();
 
-  cy.get('[name="addbutton"]').click();
+    cy.get('#meals li').eq(1).type('Falafel Wrap');
+    cy.get('#meals li:eq(1) input[type=text]').should(
+      'have.value',
+      'Falafel Wrap'
+    );
 
-  cy.get('#meals li').eq(2).type('Yakisoba');
-  cy.get('#meals li:eq(2) input[type=text]').should('have.value', 'Yakisoba');
+    cy.get('[name="addbutton"]').click();
 
-  cy.get('#meals li:eq(0) input[type=text]').clear();
-  cy.get('#meals li:eq() input[type=text]').should('have.value', '');
+    cy.get('#meals li').eq(2).type('Yakisoba');
+    cy.get('#meals li:eq(2) input[type=text]').should('have.value', 'Yakisoba');
+  });
 
-  cy.get('#meals li').eq(0).type('Tacos');
-  cy.get('#meals li:eq(0) input[type=text]').should('have.value', 'Tacos');
+  it('allows to rename the first meal', () => {
+    cy.visit('/');
+    cy.get('#meals li:eq(0) input[type=text]').clear();
+    cy.get('#meals li:eq(0) input[type=text]').should('have.value', '');
 
-  cy.get('#meals li:eq(2) input[type=checkbox]').check();
-  cy.get('#meals li:eq(2) input[type=checkbox]').should('be.checked');
-  cy.get('#meals li:eq(2) input[type=text]').should(
-    'have.css',
-    'text-decoration',
-    'line-through solid rgb(124, 128, 126)'
-  );
+    cy.get('#meals li').eq(0).type('Tacos');
+    cy.get('#meals li:eq(0) input[type=text]').should('have.value', 'Tacos');
+  });
 
-  cy.get('#meals li:eq(0) input[type=checkbox]').check();
-  cy.get('#meals li:eq(0) input[type=checkbox]').should('be.checked');
-  cy.get('#meals li:eq(0) input[type=text]').should(
-    'have.css',
-    'text-decoration',
-    'line-through solid rgb(124, 128, 126)'
-  );
+  it('allows to check and uncheck meals and strike out the checked ones', () => {
+    cy.visit('/');
 
-  cy.get('#meals li:eq(2) input[type=checkbox]').uncheck();
-  cy.get('#meals li:eq(2) input[type=checkbox]').should('not.be.checked');
+    cy.get('#meals li').eq(0).type('Burritos');
+    cy.get('[name="addbutton"]').click();
+    cy.get('#meals li').eq(1).type('Falafel Wrap');
+    cy.get('[name="addbutton"]').click();
+    cy.get('#meals li').eq(2).type('Yakisoba');
 
-  cy.get('[name="listtitle"]').clear().type('Wednesday');
-  cy.get('[name="listtitle"]').should('have.value', 'Wednesday');
+    cy.get('#meals li:eq(2) input[type=checkbox]').check();
+    cy.get('#meals li:eq(2) input[type=checkbox]').should('be.checked');
+    cy.get('#meals li:eq(2) input[type=text]').should(
+      'have.css',
+      'text-decoration',
+      'line-through solid rgb(124, 128, 126)'
+    );
+
+    cy.get('#meals li:eq(0) input[type=checkbox]').check();
+    cy.get('#meals li:eq(0) input[type=checkbox]').should('be.checked');
+    cy.get('#meals li:eq(0) input[type=text]').should(
+      'have.css',
+      'text-decoration',
+      'line-through solid rgb(124, 128, 126)'
+    );
+
+    cy.get('#meals li:eq(2) input[type=checkbox]').uncheck();
+    cy.get('#meals li:eq(2) input[type=checkbox]').should('not.be.checked');
+  });
 });
