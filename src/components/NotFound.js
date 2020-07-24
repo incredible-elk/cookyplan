88d0671f37b02a8cf9baadae069365648d3d0562
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-07-25 01:26:06 +0200

    Add 404 page and confused svg animated logo

---
+++ src/components/NotFound.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export default function NotFound() {
+  return (
+    <Styled404>
+      <img src={require('../images/logo404.svg')} />
+      <h1>0ops! Page not found. </h1>
+      <Link to="/">go back to Mealplan</Link>
+    </Styled404>
+  )
+}
+
+const Styled404 = styled.div`
+  margin: 50% 0;
+  text-align: center;
+
+  & > img {
+    padding-bottom: 20px;
+    transform: scale(3);
+  }
+
+  & > h1 {
+    margin: 20px 0;
+    font-family: var(--user);
+    color: var(--nandor);
+  }
+
+  & > a {
+    font-size: 1rem;
+    color: var(--summergreen);
+  }
+`
