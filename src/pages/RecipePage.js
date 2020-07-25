0f5133b697b9f0e8343afe00508c0d8d39217b4a
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-07-25 02:57:42 +0200

    Add recipe page with routing

---
+++ src/pages/RecipePage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import Header from '../components/Header'
+import imagePathClipboard from '../images/inverted-clipboard.svg'
+import { NavigationButton } from '../components/Button'
+import RecipeList from '../components/Recipes/RecipeList'
+
+export default function RecipePage() {
+  return (
+    <>
+      <Header>Recipes</Header>
+      <StyledMain>
+        <RecipeList />
+      </StyledMain>
+      <StyledFooter>
+        <NavigationButton to="/" as={NavLink}>
+          Mealplan
+        </NavigationButton>
+      </StyledFooter>
+    </>
+  )
+}
+
+const StyledMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+  padding-bottom: 100px;
+`
+
+const StyledFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+
+  ${NavigationButton} {
+    position: fixed;
+    background: url(${imagePathClipboard}) no-repeat center var(--summergreen);
+    background-position: left 27px top 7px;
+    text-decoration: none;
+  }
+`
