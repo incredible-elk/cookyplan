4b9c2f9f819106272b2d2f4755c40d49debad592
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-07-25 02:05:05 +0200

    Add browser router to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
 import GlobalStyles from './GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <Router>
+      <GlobalStyles />
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
