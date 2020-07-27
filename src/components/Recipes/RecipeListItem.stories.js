52f82e67170e727f2c22db887077bbef0d7801f4
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-07-27 11:34:28 +0200

    Add RecipeListItem component to storybook

---
+++ src/components/Recipes/RecipeListItem.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import RecipeListItem from './RecipeListItem'
+
+export default {
+  component: RecipeListItem,
+  title: 'RecipeListItem',
+}
+
+export const Recipe = () => <RecipeListItem title="Burritos" />
