2ad279691447d006277955e9b3b64875d1011701
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-08-05 00:12:32 +0200

    Add prop and change styles

--- src/components/Recipes/RecipeList.js
+++ src/components/Recipes/RecipeList.js
@@ -1,18 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 import recipes from '../__mocks__/recipes.json'
 import RecipeListItem from './RecipeListItem'
-import styled from 'styled-components'
 
 export default function RecipeList() {
   return (
     <StyledRecipeList>
       {recipes.map(({ id, title }) => (
-        <RecipeListItem key={id} title={title} />
+        <RecipeListItem id={id} key={id} title={title} />
       ))}
     </StyledRecipeList>
   )
 }
 
 const StyledRecipeList = styled.ul`
-  margin: 20px 0 0 10px;
+  margin-top: 10px;
 `
