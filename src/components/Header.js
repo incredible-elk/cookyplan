0d96cdaf829279466c007abb958b9eef5a6fb41f
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-08-05 00:18:20 +0200

    Add return navigation prop to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import imagePathBackArrow from '../images/back-arrow.svg'
 
-export default function Header({ children }) {
+export default function Header({ children, hasBackArrow }) {
   return (
     <StyledHeader>
+      {hasBackArrow ? (
+        <StyledLink to="/recipes">
+          <img src={imagePathBackArrow} alt="back-arrow" />
+        </StyledLink>
+      ) : null}
       <h1>{children}</h1>
     </StyledHeader>
   )
@@ -14,4 +21,11 @@ const StyledHeader = styled.header`
   justify-content: center;
   align-items: center;
   color: var(--summergreen);
+`
+
+const StyledLink = styled(Link)`
+  position: absolute;
+  top: 27px;
+  left: 20px;
+  transform: scale(1.5);
 `
