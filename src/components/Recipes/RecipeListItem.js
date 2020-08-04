70a55734ad784bfabed3c89ba8cb574d7be01dfa
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-08-05 00:13:19 +0200

    Add routing and styles to listitem

--- src/components/Recipes/RecipeListItem.js
+++ src/components/Recipes/RecipeListItem.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-export default function RecipeListItem({ title }) {
-  return <StyledRecipeListItem>{title}</StyledRecipeListItem>
+export default function RecipeListItem({ id, title }) {
+  return (
+    <StyledRecipeListItem>
+      <StyledLink to={`/recipes/${id}`}>{title}</StyledLink>
+    </StyledRecipeListItem>
+  )
 }
 
 const StyledRecipeListItem = styled.li`
   display: flex;
   flex-direction: row;
-  margin: 10px 0 0 30px;
+  margin: 0 30px;
+  padding: 15px 0;
   list-style: none;
   font-family: var(--user);
+  border-bottom: 1px solid var(--divider);
   color: var(--nandor);
+  text-decoration: none;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-family: var(--user);
+  color: inherit;
 `
