137fc3b3bb207d955456536300c0e4d94cda666f
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-08-05 00:19:00 +0200

    Add recipe detail component

---
+++ src/components/Recipes/RecipeDetail.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function RecipeDetail({ ingredients, method, title }) {
+  return (
+    <>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledHeader>Ingredients:</StyledHeader>
+      <StyledDetails>
+        {ingredients.map((ingredient) => (
+          <>
+            {ingredient}
+            <br />
+          </>
+        ))}
+      </StyledDetails>
+      <StyledHeader>Method:</StyledHeader>
+      <StyledDetails>
+        {method.split('\n').map((paragraph) => (
+          <p>{paragraph}</p>
+        ))}
+      </StyledDetails>
+    </>
+  )
+}
+
+const StyledTitle = styled.h2`
+  margin: 0 20px 0 25px;
+  font-size: 2rem;
+  font-family: var(--user);
+  color: var(--input);
+`
+
+const StyledHeader = styled.h3`
+  margin: 30px 0 5px 25px;
+  font-size: 1.3rem;
+  color: var(--summergreen);
+`
+
+const StyledDetails = styled.p`
+  margin: 0 auto;
+  width: 80%;
+  height: auto;
+  font-family: var(--user);
+  color: var(--input);
+`
