14e70a737b1f335f02882145aa9e6b944597b580
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-08-05 13:19:09 +0200

    Add navigation to recipe detail page to test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,6 +6,16 @@ describe('Navigation', () => {
     cy.url().should('match', /recipes/);
 
     cy.get('[name="navbutton"]').click();
-    cy.url().should('include', '/');
+    cy.url().should('equal', 'http://localhost:3000/');
+  });
+
+  it('navigates between recipe page and recipe detail page', () => {
+    cy.visit('/recipes');
+
+    cy.get('#recipes li:eq(6) a').click();
+    cy.url().should('equal', 'http://localhost:3000/recipes/7');
+
+    cy.get('[data-testid="back-arrow"]').click();
+    cy.url().should('match', /recipes/);
   });
 });
