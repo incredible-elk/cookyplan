69c58cfc5e1baa51cfd1e18c09c06624658afd0b
incredible-elk <37314329+incredible-elk@users.noreply.github.com>
2020-08-12 16:47:02 +0200

    Rename test and data-testid

--- cypress/integration/addOrRemoveFromMealplan.spec.js
+++ cypress/integration/addOrRemoveFromMealplan.spec.js
@@ -1,9 +1,9 @@
-describe('Add and remove recipe to Mealplan', () => {
+describe('Add and remove recipe from Mealplan', () => {
   it('Adds recipe to Mealplan ', () => {
     cy.visit('/recipes');
 
     cy.get('#recipes li:eq(3) a').click();
-    cy.get('[data-testid="add-to-mealplan"]').click();
+    cy.get('[data-testid="add-or-remove-from-mealplan"]').click();
 
     cy.visit('/');
     cy.get('#meals li').eq(0);
@@ -18,14 +18,14 @@ describe('Add and remove recipe to Mealplan', () => {
 
     cy.visit('/recipes');
     cy.get('#recipes li:eq(4) a').click();
-    cy.get('[data-testid="add-to-mealplan"]').click();
+    cy.get('[data-testid="add-or-remove-from-mealplan"]').click();
 
     cy.visit('/recipes');
 
     cy.get('#recipes li:eq(3) a').click();
-    cy.get('[data-testid="add-to-mealplan"]').click();
+    cy.get('[data-testid="add-or-remove-from-mealplan"]').click();
 
-    cy.get('[data-testid="add-to-mealplan"]').click();
+    cy.get('[data-testid="add-or-remove-from-mealplan"]').click();
 
     cy.visit('/');
     cy.get('#meals li:eq(0) input[type=text]').should(
