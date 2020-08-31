4d8d8eecb3adb0e600b7faa54a7761745d8f56ca
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2020-08-31 15:55:44 +0200

    Delete unused imports

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,7 +1,7 @@
+import { render } from '@testing-library/react'
 import React from 'react'
-import Header from './Header'
-import { render, getByTestId } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
+import Header from './Header'
 
 describe('Header.test.js', () => {
   it('renders children prop into a h1 tag', () => {
